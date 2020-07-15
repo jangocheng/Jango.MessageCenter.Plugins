247db2af389ef57967d0df7894ad2c66c7650e1f
jangocheng <35371233+jangocheng@users.noreply.github.com>
2020-07-15 11:49:12 +0800

    init

---
+++ Jango.RabbitMq/Dequeue.cs
@@ -0,0 +1,46 @@
+﻿using EasyNetQ;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jango.MessageCenter.RabbitMq
+{
+    public abstract class Dequeue : IDequeue
+    {
+        private readonly IBus _bus;
+        private string _queueName;
+        public Dequeue(string connString, string queueName)
+        {
+            _bus = RabbitHutch.CreateBus(connString);
+            _queueName = queueName;
+        }
+        public Task Dispose()
+        {
+            return Task.Run(() =>
+            {
+                _bus.Dispose();
+            });
+        }
+
+        public abstract Task ProcessMessage(string msg);
+
+        async Task IDequeue.Dequeue()
+        {
+            _bus.Receive<IMessage>(_queueName, (msg) =>
+            {
+                OnMessage(msg);
+            });
+
+        }
+
+        private void OnMessage(IMessage message)
+        {
+            var msg = JsonConvert.SerializeObject(message);
+            ProcessMessage(msg);
+        }
+
+
+    }
+}
---
+++ Jango.RabbitMq/Enqueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyNetQ;
+
+namespace Jango.MessageCenter.RabbitMq
+{
+    public class Enqueue : IEnqueue
+    {
+        private readonly IBus _bus;
+        private string _queueName;
+        public Enqueue(string connString, string queueName)
+        {
+            _queueName = queueName;
+            _bus = RabbitHutch.CreateBus(connString);
+        }
+        async Task IEnqueue.Enqueue(IMessage msg)
+        {
+            await _bus.SendAsync(_queueName, msg);
+        }
+
+        async Task IEnqueue.Enqueue(IEnumerable<IMessage> msgs)
+        {
+            await _bus.SendAsync(_queueName, msgs);
+        }
+
+        async Task<long> IEnqueue.ScheduledEnqueue(IMessage msg, System.DateTimeOffset timeOffset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
